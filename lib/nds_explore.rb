d98a7f969fc1eb978ad5d9bb901af384cbe5a5d9
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-25 04:23:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,8 +9,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  dir_movies = directors_database[0][:movies]
   movies_index = 0
-  while movies_index < directors_database[0][:movies].length do
+  while movies_index < dir_movies.length do
     p directors_database[0][:movies][movies_index][:title]
     movies_index += 1
   end
